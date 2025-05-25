36f76694a0d73f07a8c642673bffe420b4a0b380
kotbk <kpas95545@gmail.com>
2025-05-26 00:55:01 +0400

    save_to_file

--- src/save_to_file.py
+++ src/save_to_file.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger('save_to_file')
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs/save_to_file.log', encoding='windows-1251')
+file_handler = logging.FileHandler('logs/save_to_file.log', encoding='utf-8')
 file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
