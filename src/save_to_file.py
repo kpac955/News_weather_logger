0ed3642b487cf7470a88cd485e1a21ec6005b9db
kotbk <kpas95545@gmail.com>
2025-05-27 00:27:03 +0400

    save_to_file

--- src/save_to_file.py
+++ src/save_to_file.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger('save_to_file')
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs/save_to_file.log', encoding='utf-8')
+file_handler = logging.FileHandler('logs/save_to_file.log', encoding="cp1251")
 file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
@@ -13,7 +13,7 @@ def save_to_file(data: list, file_path: str) -> None:
     """сохранение данных по указанному пути"""
     try:
         logger.info(f'Записываем данные в файл {file_path}')
-        with open(file_path, 'w') as data_file:
-            json.dump(data, data_file)
+        with open(file_path, 'w', encoding='utf-8') as data_file:
+            json.dump(data, data_file, ensure_ascii=False)
     except Exception as ex:
         logger.error(f'Произошла ошибка: {ex}')
